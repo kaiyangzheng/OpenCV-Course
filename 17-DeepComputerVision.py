d3c5b4031d10f499439bb26575e2197871151c83
Kaiyang Zheng <kaiyangzheng@Kaiyangs-MacBook-Pro.local>
2022-06-18 15:47:04 -0700

    6/18/22

---
+++ 17-DeepComputerVision.py
@@ -0,0 +1,9 @@
+"""
+Deep Computer Vision Model
+- opencv is generally using for preprocessing the image data
+    - image normalization, mean subtraction, etc
+- keras is generally used for building the neural network
+    - has a lot of boilerplate code to setup the model
+"""
+
+# ! unsupported on m1 mac
